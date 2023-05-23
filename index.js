ea3acc3108cb1609986f9d25cc5a56f655c83d79
Sirius <siriusvllos@gmail.com>
2023-05-23 11:26:58 -0300

    td funcionando ate o route 53

--- index.js
+++ index.js
@@ -2,25 +2,19 @@
 const pulumi = require("@pulumi/pulumi");
 const aws = require("@pulumi/aws");
 const awsx = require("@pulumi/awsx");
-// tentar remover linhas 1 & 4 dps q estiver funcionando
 
 const deployer = new aws.ec2.KeyPair("deployer", {
   publicKey:
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPU0SkQsSV1nUypP4lRJ2ASTDZCCpFSMNu06pPEoU4Ry siriusvzevallos@ip-10-0-1-207.sa-east-1.compute.internal",
 });
-// S3 BUCKET --- IMAGES
 const bucket = new aws.s3.Bucket("images");
 exports.bucketName = bucket.id;
 
-// VPC --- MAIN
-// https://www.pulumi.com/docs/clouds/aws/guides/vpc/
 const vpc = new awsx.ec2.Vpc("main");
 const vpcId = vpc.vpcId;
 const privateSubnetIds = vpc.privateSubnetIds;
 const publicSubnetIds = vpc.publicSubnetIds;
 
-// EC2 --- BASTION
-// https://github.com/pulumi/examples/tree/master/aws-js-webserver
 let size = "t2.micro"; // eh free ta danado :)
 
 let ami = aws.ec2.getAmi({
@@ -34,12 +28,11 @@ let ami = aws.ec2.getAmi({
       values: ["hvm"],
     },
   ],
-  // aws ec2 describe-images --owner "aws-marketplace" --filters "Name=name,Values=ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*" "Name=virtualization-type,Values=hvm" --region "sa-east-1"
+  //aws ec2 describe-images --owner "aws-marketplace" --filters "Name=name,Values=ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*" "Name=virtualization-type,Values=hvm" --region "sa-east-1"
   owners: ["aws-marketplace"],
   mostRecent: true,
 });
 
-// EC2 SECURITY GROUP --- MAIN
 let group = new aws.ec2.SecurityGroup("main", {
   vpcId: vpcId,
   ingress: [
@@ -51,10 +44,19 @@ let server = new aws.ec2.Instance("webserver-www", {
   instanceType: size,
   keyName: deployer.keyName,
   vpcSecurityGroupIds: [group.id],
-  subnetId: privateSubnetIds[1],
-  // ami: "ami-0b7af114fb404cd23",
+  subnetId: publicSubnetIds[1],
   ami: ami.then((ami) => ami.name),
+}); // o ip desse cara* vai dentro do route53
+
+const bastion = new aws.route53.Record("bastion", {
+  // isirius.link ID ----->>>>> Z02274863NULTFNYSASLO :) <#
+  // registro tipo A  ---> https://bastion.isirius.link/
+  zoneId: "Z02274863NULTFNYSASLO",
+  name: "bastion",
+  type: "A",
+  ttl: 300,
+  records: [server.publicIp], //desse cara*
 });
 
-exports.publicIp = server.publicIp;
+exports.publicIp = server.publicIp; // desse cara*
 exports.publicHostName = server.publicDns;
