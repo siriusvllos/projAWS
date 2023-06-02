f73746b672c586fe0800e019a6dea16f4be2eef5
Sirius <siriusvllos@gmail.com>
2023-06-02 11:40:46 -0300

    yyy

--- index.js
+++ index.js
@@ -37,8 +37,87 @@ let group = new aws.ec2.SecurityGroup("main", {
   vpcId: vpcId,
   ingress: [
     { protocol: "tcp", fromPort: 22, toPort: 22, cidrBlocks: ["0.0.0.0/0"] },
+    {
+      protocol: "tcp",
+      fromPort: 3030,
+      toPort: 3030,
+      cidrBlocks: ["0.0.0.0/0"],
+    },
+  ],
+  egress: [
+    { protocol: "tcp", fromPort: 22, toPort: 22, cidrBlocks: ["0.0.0.0/0"] },
+    { protocol: "tcp", fromPort: 80, toPort: 80, cidrBlocks: ["0.0.0.0/0"] },
+    { protocol: "tcp", fromPort: 443, toPort: 443, cidrBlocks: ["0.0.0.0/0"] },
   ],
 });
+
+// -------------------- CONST USER DATA -----------------------------
+
+const userData = `#!/bin/bash
+set -x
+
+sudo apt update
+sudo apt install nodejs -y
+sudo apt install git -y
+sudo apt install npm -y
+
+curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+sudo apt update
+sudo apt install mongodb-org -y
+sudo systemctl start mongod.service
+sudo rm /etc/mongod.conf
+
+cat >'/home/ubuntu/.ssh/id_ed25519' <<EOT
+-----BEGIN OPENSSH PRIVATE KEY-----
+b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZW
+QyNTUxOQAAACDqAVUdlFK42+RdcClBPZKzCxYExndvgHpQy76NwEf/0wAAAJhml3lTZpd5
+UwAAAAtzc2gtZWQyNTUxOQAAACDqAVUdlFK42+RdcClBPZKzCxYExndvgHpQy76NwEf/0w
+AAAEBoWvJiXcF2mqNLQhmqcIUbY3Dn284TZ55KB2U/y1irxeoBVR2UUrjb5F1wKUE9krML
+FgTGd2+AelDLvo3AR//TAAAAFXNpcml1c3ZsbG9zQGdtYWlsLmNvbQ==
+-----END OPENSSH PRIVATE KEY-----
+EOT
+
+cat >'/etc/mongod.conf' << EOT 
+storage:
+  dbPath: /var/lib/mongodb
+  journal:
+    enabled: true
+systemLog:
+  destination: file
+  logAppend: true
+  path: /var/log/mongodb/mongod.log
+net:
+  port: 44805
+  bindIp: 127.0.0.1
+processManagement:
+  timeZoneInfo: /usr/share/zoneinfo
+EOT
+sudo service mongod restart
+
+cat >'/home/ubuntu/.ssh/config' <<EOT
+Host github.com            
+   StrictHostKeyChecking no
+   UserKnownHostsFile=/dev/null    
+EOT
+
+sudo chmod 700 /home/ubuntu/.ssh
+sudo chmod 600 /home/ubuntu/.ssh/id_ed25519
+sudo chown ubuntu:ubuntu -R /home/ubuntu/.ssh
+
+mkdir /home/ubuntu/app
+sudo chown ubuntu:ubuntu -R /home/ubuntu/app
+
+id
+sudo -u ubuntu git clone git@github.com:siriusvllos/catinder.git /home/ubuntu/app/catinder
+
+cd /home/ubuntu/app/catinder
+npm install
+`;
+// sudo npm install express ----> instalar todos os pacotes q o catinder requer instead
+// tee le uma coisa do teclado e grava num arquivo q eu passei como param
+
+// ------------------- FIM USER DATA -------------------------------
 
 let server = new aws.ec2.Instance("webserver-www", {
   instanceType: size,
@@ -46,6 +125,7 @@ let server = new aws.ec2.Instance("webserver-www", {
   vpcSecurityGroupIds: [group.id],
   subnetId: publicSubnetIds[1],
   ami: ami.then((ami) => ami.id),
+  userData: userData,
 }); // o ip desse cara* vai dentro do route53
 
 const bastion = new aws.route53.Record("bastion", {
